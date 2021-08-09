625cc11c6d5436d8e873ad74ce45e2c68a7fc46a
Payam Kelich <payam.kelich68@gmail.com>
2021-08-09 12:34:16 -0600

    new release

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name='binderspace',
     packages=['binderspace'],
-    version='0.1',
+    version='0.5',
     license='mit',
     description='a library for finding the space for binding sequences',
     author='Vukovic Lab (Payam Kelich)',
     author_email='lvukovic@utep.edu',  # Type in your E-Mail
     url='https://github.com/vukoviclab/BinderSpace',  # Provide either the link to your github or to your website
-    download_url='https://github.com/vukoviclab/BinderSpace/archive/refs/tags/0.1.tar.gz',
+    download_url='https://github.com/vukoviclab/BinderSpace/archive/refs/tags/0.5.tar.gz',
     keywords=['DNA', 'sequence', 'pca', 't-sne', 'peptide'],  # Keywords that define your package best
     install_requires=[
         'scikit-learn',
